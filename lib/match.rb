f7ba4da8811d9dbc9527cf9946633a409bc19ba2
violake <violake@foxmail.com>
2019-11-03 14:54:25 +1100

    add test and methods for tie break and match over.

--- lib/match.rb
+++ lib/match.rb
@@ -13,13 +13,17 @@ class Match
   end
 
   def point_won_by(player_name)
+    raise StandardError, 'Match is over!' if set_complete?
+
     add_game_point(players.index(player_name))
 
     update_set
   end
 
   def score
-    if current_game
+    if set_complete?
+      set.join('-') + ", #{players[set.index(set.max)]} win"
+    elsif current_game
       set.join('-') + ', ' + current_game_to_s
     else
       set.join('-')
@@ -40,8 +44,20 @@ class Match
     end
   end
 
+  def set_complete?
+    set.max >= 6 && (set[0] - set[1]).abs > 1 || set.max == 7
+  end
+
+  def tie_break?
+    set.uniq.count == 1 && set[0] == 6
+  end
+
   def current_game_complete?
-    current_game && current_game.max >= 4 && (current_game[0] - current_game[1]).abs > 1
+    tie_break? ? game_complete?(7) : game_complete?(4)
+  end
+
+  def game_complete?(game_point)
+    current_game && current_game.max >= game_point && (current_game[0] - current_game[1]).abs > 1
   end
 
   def create_new_game
@@ -50,6 +66,10 @@ class Match
   end
 
   def current_game_to_s
+    tie_break? ? current_game.join('-') : game_to_s
+  end
+
+  def game_to_s
     if current_game.uniq.count == 1 && current_game[0] >= 3
       'Deuce'
     elsif current_game.max > 3 && (current_game[0] - current_game[1]).abs == 1
--- spec/unit/match_spec.rb
+++ spec/unit/match_spec.rb
@@ -125,6 +125,105 @@ describe Match do
         expect(subject.score).to eq '2-3, 0-15'
       end
     end
+
+    context 'player 1 win the match' do
+      before do
+        player_won_games(subject, player_1, 5)
+        player_won_games(subject, player_2, 3)
+        player_won_games(subject, player_1, 1)
+      end
+
+      it { expect(subject.score).to eq '6-3, player 1 win' }
+    end
+  end
+
+  context 'tie break' do
+    before do
+      player_won_games(subject, player_1, 5)
+      player_won_games(subject, player_2, 6)
+      player_won_games(subject, player_1, 1)
+    end
+
+    context 'player 1 won 1 point' do
+      before do
+        player_won_points(subject, player_1, 1)
+      end
+
+      it 'should get tie break 1-0 game score' do
+        expect(subject.score).to eq '6-6, 1-0'
+      end
+    end
+
+    context 'player 2 won 1 point' do
+      before do
+        player_won_points(subject, player_2, 1)
+      end
+
+      it 'should get tie break 0-1 game score' do
+        expect(subject.score).to eq '6-6, 0-1'
+      end
+    end
+
+    context 'only one player won 6 points and won next point' do
+      before do
+        player_won_points(subject, player_1, 6)
+        player_won_points(subject, player_2, 5)
+        player_won_points(subject, player_1, 1)
+      end
+
+      it 'should get 7-6 set score and game over' do
+        expect(subject.score).to eq '7-6, player 1 win'
+      end
+    end
+
+    context 'both players won 6 points and one player won next point' do
+      before do
+        player_won_points(subject, player_1, 6)
+        player_won_points(subject, player_2, 6)
+        player_won_points(subject, player_1, 1)
+      end
+
+      it 'should get 7-6 game score' do
+        expect(subject.score).to eq '6-6, 7-6'
+      end
+    end
+
+    context 'both players won 6 points and both player won 1 point' do
+      before do
+        player_won_points(subject, player_1, 6)
+        player_won_points(subject, player_2, 6)
+        player_won_points(subject, player_1, 1)
+        player_won_points(subject, player_2, 1)
+      end
+
+      it 'should get 7-7 game score' do
+        expect(subject.score).to eq '6-6, 7-7'
+      end
+    end
+
+    context 'both players won 6 points and one player won next 2 points' do
+      before do
+        player_won_points(subject, player_1, 6)
+        player_won_points(subject, player_2, 6)
+        player_won_points(subject, player_2, 2)
+      end
+
+      it 'should get 6-7 set score and game over' do
+        expect(subject.score).to eq '6-7, player 2 win'
+      end
+    end
+  end
+
+  context 'Match is over' do
+    before do
+      player_won_games(subject, player_1, 3)
+      player_won_games(subject, player_2, 6)
+    end
+
+    it 'should raise error' do
+      expect { subject.point_won_by(player_1) }
+        .to raise_error(StandardError, 'Match is over!')
+    end
   end
 
   private
